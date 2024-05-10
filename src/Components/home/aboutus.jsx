473543bbb41669dfef8866075ae8d893f5170ad2
HamzaHamani <hamzahamani2004@gmail.com>
2024-05-10 20:18:19 +0100

    feat: Update About Us section on home page
    This commit updates the About Us section on the home page. It adds a new container with a white background and black text, containing a description and a call-to-action for users to seek help. The section now has improved styling and layout.

--- src/Components/home/aboutus.jsx
+++ src/Components/home/aboutus.jsx
@@ -1,6 +1,6 @@
 function Aboutus() {
   return (
-    <section className="bg-primary mt-28 p-10  text-white">
+    <section className="bg-primary mt-28 p-10 h-screen  text-white">
       <h2 className="text-3xl text-accent flex gap-6 items-center mb-8">
         <span className="text-6xl">-</span> WHO WE ARE
       </h2>
@@ -22,14 +22,23 @@ function Aboutus() {
           </div>
           <button>Read More</button>
         </div>
-        <div className="bg-red-200">
-          <div className="w-[626px] h-[600px] bg-slate-600 object-contain">
+        <div className="relative">
+          <div className="w-[626px] h-[600px] bg-slate-600 object-contain rounded-lg overflow-hidden relative">
             <img
               src="../../../public/contact.jpg"
               className="object-contain imgfit"
             />
           </div>
-          <div></div>
+          <div className="bg-white text-black  flex w-[485px] h-[286px] mx-auto p-8 rounded-lg flex-col items-center justify-evenly absolute -bottom-24 right-20">
+            <p className="text-primary  font-medium">
+              Lorem Ipsum is simply dummy
+            </p>
+            <h2 className="text-2xl font-medium -mt-3">Looking for help?</h2>
+            <p className=" w-[80%] self-center text-center -mt-3 text-gray-900">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
         </div>
       </div>
     </section>
