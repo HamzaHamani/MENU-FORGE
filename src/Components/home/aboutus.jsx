668adb6e850c527f763946361c051eb5feb441b2
HamzaHamani <hamzahamani2004@gmail.com>
2024-05-11 16:38:46 +0100

    refactor: Improve styling and layout of About Us section on home page

--- src/Components/home/aboutus.jsx
+++ src/Components/home/aboutus.jsx
@@ -6,21 +6,49 @@ function Aboutus() {
       </h2>
       <div className="flex gap-10 ">
         <div className="flex-grow w-[30%]">
-          <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-14 mt-16">
             <h2 className="text-6xl flex-grow w-fit ">
               We Help to Get Solutions
             </h2>
-            <p className=" text-lg w-[50%]">
+            <p className=" text-2xl text-gray-200 w-[78%] mb-20">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industrys standard dummy text
               ever since the 1500s,asdasdasdasdasd
             </p>
           </div>
-          <div className="bg-stone-600 w-fit">
-            <div>icon1</div>
-            <div>icon2</div>
+          <div className="w-fit flex">
+            <div className="flex items-center justify-center gap-6">
+              <img
+                src="../../../public/rocket.svg"
+                alt="rocket svg icon"
+                className="scale-90"
+              />
+              <div className="flex flex-col gap-4">
+                <h2 className="text-4xl">Lorem Ipsum</h2>
+                <p className="w-[75%] text-gray-200 text-base">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center justify-center gap-6">
+              <img
+                src="../../../public/idea.svg"
+                alt="idea svg icon"
+                className="scale-90"
+              />
+              <div className="flex flex-col gap-4">
+                <h2 className="text-4xl">Lorem Ipsum</h2>
+                <p className="w-[75%] text-gray-200 text-base">
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.
+                </p>
+              </div>
+            </div>
           </div>
-          <button>Read More</button>
+          <button className="bg-accent p-10 py-3 text-xl text-black">
+            Read More
+          </button>
         </div>
         <div className="relative">
           <div className="w-[626px] h-[600px] bg-slate-600 object-contain rounded-lg overflow-hidden relative">
@@ -30,10 +58,10 @@ function Aboutus() {
             />
           </div>
           <div className="bg-white text-black  flex w-[485px] h-[286px] mx-auto p-8 rounded-lg flex-col items-center justify-evenly absolute -bottom-24 right-20">
-            <p className="text-primary  font-medium">
-              Lorem Ipsum is simply dummy
-            </p>
-            <h2 className="text-2xl font-medium -mt-3">Looking for help?</h2>
+            <p className=" text-lg  font-medium">Lorem Ipsum is simply dummy</p>
+            <h2 className="text-3xl font-medium -mt-3 text-primary">
+              Looking for help?
+            </h2>
             <p className=" w-[80%] self-center text-center -mt-3 text-gray-900">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.
