c365ccc836b4afe8faeaa7e39335f5448df4d320
HamzaHamani <hamzahamani2004@gmail.com>
2024-05-17 17:43:38 +0100

    refactor: Update footer component with new styles and content
    The footer component has been updated with new styles and content. Added the "text-white" class to the footer element to change the text color to white. Updated the "Discover whats new every month at menuforge.com" text to use the "text-black" class for better visibility. Adjusted the layout and spacing of the footer sections. This refactor improves the aesthetics and usability of the footer component.

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <footer className="h-screen">
+    <footer className="h-screen text-white">
       <div className="mt-2  p-10"></div>
       <div className="bg-secondary2 p-6">
-        <p className="text-center underline ">
+        <p className="text-center text-black underline ">
           Discover whats new every month at menuforge.com
         </p>
       </div>
@@ -13,32 +13,39 @@ function Footer() {
             <img src="../../public/logoWhite.png" width={100} />
 
             <p className="text-primaryLighter font-medium">
-              Copyright @2024
-              <a href="#" className="underline">
+              Copyright @2024{" "}
+              <a href="#" className="cursor-pointer underline">
                 MenuForge
-              </a>{" "}
+              </a>
             </p>
-            <p className="text-white">Terms of Service</p>
           </div>
-          <div className="flex justify-around gap-28">
-            <div>
-              <h3 className="text-primaryLighter font-medium">Company</h3>
+          <div className="flex justify-end gap-10">
+            <div className="flex w-[35%] flex-col gap-8">
+              <h3 className="w-fit text-lg font-medium text-accent">About</h3>
+              <p className=" text-sm">
+                Menu Forge lets restaurants create and showcase digital menus
+                with ease.
+              </p>
             </div>
-            <div>
-              <h3 className="text-primaryLighter font-medium">Customer</h3>
+            <div className="flex flex-col gap-8">
+              <h3 className="text-primaryLighter text-lg font-medium">
+                Social
+              </h3>
+              <ul className="flex flex-col gap-3">
+                <li>Instagram</li>
+                <li>Linkedin</li>
+              </ul>
             </div>
           </div>
         </div>
         <div className="flex w-[10000px] flex-col  overflow-hidden bg-yellow-50 text-secondary2">
           <div className=" absolute -bottom-10  -ml-3 text-[18rem] font-bold capitalize">
-            <p className="font-spacemono absolute -top-10 left-52 text-[6rem] font-semibold uppercase tracking-[0.5em]">
+            <p className="font-spacemono absolute -top-10 left-56 text-center text-[6rem] font-semibold uppercase tracking-[0.5em] ">
               {" "}
               Dont waste the{" "}
             </p>
 
-            <span className="font-spacemono text-primaryLighter">
-              opportunity
-            </span>
+            <span className="font-spacemono ">opportunity</span>
           </div>
         </div>
       </section>
